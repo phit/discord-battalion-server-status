58264e7d92f4bc902f23988c940240a3ff8dbd0a
phit <phit@hush.com>
2018-02-03 16:23:00 +0100

    comments!

--- app.js
+++ app.js
@@ -25,21 +25,28 @@ client.on('message', message => {
               helpembed = new Discord.RichEmbed()
                 .setColor(5151967)
                 .addField("Server Status Checker for Battalion1944", "Usage: `" + command + " <IP>[:port] [password]`" +
-                    "\n*Port is optional and defaults to 7777+3, password is optional and purely cosmetic only works on passworded servers*")
+                    "\n\n*Optional port is the Query port NOT gameserver port, defaults to 7777+3*\n*Optional password is purely cosmetic only works for passworded servers*")
                 .addField("Author", "phit#4970")
                 .setTimestamp();
               message.reply("", {embed: helpembed});
         } else {
+            // get rid of spaces around
             arguments = message.content.substr(command.length).trim();
+            // split arguments
             aarguments = arguments.split(" ");
             if (aarguments.length == 0) {
                 aarguments.push(arguments);
             }
             
+            // split address for port and ip
             aaddresss = aarguments[0].split(":");
             ip = aaddresss[0];
             port = aaddresss[1] || 7780;
+            
+            // check for existence of cosmetic password
             password = aarguments[1] || "";
+            
+            // lets check if its up
             getStatus(ip, port)
                 .then((response) => {
                     // Playerlist doesn't seem to work currently..
@@ -56,6 +63,7 @@ client.on('message', message => {
                         // }
                     // }
                     
+                    // server is passworded, adding password to connect command
                     if (response.raw.rules.bat_has_password_s == "Y") {
                         if (password != "") {
                             password = " password " + password;
@@ -64,6 +72,7 @@ client.on('message', message => {
                         }
                     }
 
+                    // build reply embed
                     richembed = new Discord.RichEmbed()
                         .setColor(7844437)
                         .setThumbnail("https://phit.link/battalion/" + response.raw.rules.bat_map_s.replace(/ /g,"_").toLowerCase() + ".png")
@@ -74,16 +83,22 @@ client.on('message', message => {
                             "\nMap: " + response.raw.rules.bat_map_s +
                             "\nMode: " + response.raw.rules.bat_gamemode_s +
                             "\n\n`connect " + response.query.host + ":" + response.raw.port + password + "`");
+
+                    // reply!
                     message.reply("", {embed: richembed});
                     console.log("[" + ip + "] replied online!");
                 }).catch((error) => {
+                    // error in query
                     console.error(error);
+                    
+                    // assume user error
                     richembed = new Discord.RichEmbed()
                         .setColor(16711680)
                         .setTimestamp()
                         .addField(ip + " :x:",
-                            "Server is offline or address is invalid..");
+                            "Server is offline or address is invalid..\nIf you entered a port, remember to use the Query port.");
 
+                    // reply!
                     message.reply("", {embed: richembed});
                     console.log("[" + ip + "] replied offline!");
                 });
@@ -92,8 +107,10 @@ client.on('message', message => {
 });
 
 function getStatus(ip, port) {
+    // fallback to default port
     port = port || 7780;   
     console.log("[" + ip + "] getting status for " + ip + ":" + port)
+    // run the query
     return gamedig.query({
         type: 'protocol-valve',
         socketTimeout: 2000,
